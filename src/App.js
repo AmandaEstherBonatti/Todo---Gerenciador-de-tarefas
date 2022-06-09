7cce537f2ad96b12b28777f3dd29d34684bd04fe
Amanda Esther Bonatti <estudosbonatti@gmail.com>
2022-06-09 16:49:10 -0300

    continuand o curso

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-import {BrTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs"
+import { useState, useEffect } from 'react';
+import { BrTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs"
 
 const API = "http://localhost:5000/";
 
@@ -8,18 +8,53 @@ function App() {
   const [title, setTitle] = useState("")
   const [time, setTime] = useState("")
   const [todos, setTodos] = useState([])
-  const [loading, setLoading] =  useState(false)
-  
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+
+    console.log(todo)
+    setTitle("");
+    setTime("");
+  };
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>React Todo</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você está planejando realizar?</label>
+            <input type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""} required />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""} required />
+          </div>
+          <input type="submit" value="Criar Tarefa" />
+        </form>
       </div>
       <div className="list-todo">
-        <p>Lista</p>
+        <p>Lista de tarefas:</p>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
       </div>
     </div>
   );
